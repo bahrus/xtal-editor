0e98b57878966704a2454a6b0f8463c4eb06e00f
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-09-25 09:12:06 -0400

    ...

--- src/xtal-editor.ts
+++ src/xtal-editor.ts
@@ -11,12 +11,12 @@ import('tran-sister/tran-sister.js');
 // const style = await import('./theme.css', {
 //     assert: { type: 'css' }
 // });
-const splitPath = import.meta.url.split('/');
-splitPath.pop();
-const rel = splitPath.join('/');
-const cssPath = rel + '/theme.css';
+// const splitPath = import.meta.url.split('/');
+// splitPath.pop();
+// const rel = splitPath.join('/');
+// const cssPath = rel + '/theme.css';
 
-//const cssPath = 'https://unpkg.com/xtal-editor@0.0.58/src/theme.css'
+const cssPath = 'https://unpkg.com/xtal-editor@0.0.63/src/theme.css'
 const mainTemplate = tm.html`
 <link rel=stylesheet href=${cssPath}>
 <slot part=slot name=initVal></slot>
