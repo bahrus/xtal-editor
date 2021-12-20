fffbf5d3acc0fd5309726f8012f5e5c3fbf2c9aa
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-12-20 17:16:47 -0500

    0.0.108

--- xtal-editor.ts
+++ xtal-editor.ts
@@ -3,12 +3,7 @@ import {DefineArgs} from 'xtal-element/src/types';
 import { PropInfoExt, XE } from 'xtal-element/src/XE.js';
 import {importJSON} from 'be-loaded/importJSON.js';
 import('be-loaded/be-loaded.js');
-import('be-observant/be-observant.js');
-import('be-switched/be-switched.js');
-import('xtal-side-nav/xtal-side-nav.js');
-import('be-transformative/be-transformative.js');
-import('be-deslotted/be-deslotted.js');
-import('@power-elements/json-viewer/json-viewer.js');
+
 import('./xtal-editor-field.js');
 import {tm, TemplMgmtProps, TemplMgmtActions} from 'trans-render/lib/mixins/TemplMgmtWithPEST.js';
 
