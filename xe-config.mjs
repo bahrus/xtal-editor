1839b38fcc9414a941894187fc9c31752d426141
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-08-28 20:52:01 -0400

    0.0.191

--- xe-config.mjs
+++ xe-config.mjs
@@ -47,12 +47,8 @@ const innerHTML = html `
     <script data-version=0.0.67  id=be-noticed/be-noticed.js></script>
     <script data-version=0.0.132 id=be-observant/be-observant.js></script>
     <script data-version=0.0.78  id=be-switched/be-switched.js></script>
-    <script data-version=0.0.16  id=be-lazy/be-lazy.js></script>
-    <script data-version=0.0.87  id=xtal-side-nav/xtal-side-nav.js></script>
     <script data-version=0.0.46  id=be-transformative/be-transformative.js></script>
     <script data-version=0.0.34  id=be-deslotted/be-deslotted.js></script>
-    <script data-version=0.0.162 id=xtal-tree/xtal-tree.js></script>
-    <script data-version=0.0.75  id=xtal-vlist/xtal-vlist.js></script>
     <script data-version=0.0.1   id=be-composed/be-composed.js></script>
 </template>
 <style ${{
@@ -68,8 +64,13 @@ main{
 </style>
 <slot name=init-val be-deslotted='["value"]'></slot>
 <main part=main>
-
-<xtal-tree id-path=path ${{
+    <template be-active>
+        <script data-version=0.0.162 id=xtal-tree/xtal-tree.js></script>
+        <script data-version=0.0.75  id=xtal-vlist/xtal-vlist.js></script>
+        <script data-version=0.0.16  id=be-lazy/be-lazy.js></script>
+        <script data-version=0.0.87  id=xtal-side-nav/xtal-side-nav.js></script>
+    </template>
+    <xtal-tree id-path=path ${{
     beObservant: [{
             objectGraph: {
                 onSet: 'value',
@@ -89,11 +90,11 @@ main{
         }
     }
 }}></xtal-tree>
-</div>
-<header part=header>
-    <xtal-side-nav part=side-nav>
-        <menu part=menu>
-            <button class="selector text-view-selector" part=text-view-selector ${{
+    </div>
+    <header part=header>
+        <xtal-side-nav part=side-nav>
+            <menu part=menu>
+                <button class="selector text-view-selector" part=text-view-selector ${{
     beTransformative: {
         click: {
             transform: {
@@ -104,7 +105,7 @@ main{
         }
     }
 }}>Text View</button>
-            <button class="selector tree-view-selector inactive" part=tree-view-selector ${{
+                <button class="selector tree-view-selector inactive" part=tree-view-selector ${{
     beTransformative: {
         click: {
             transform: {
@@ -115,8 +116,8 @@ main{
         }
     }
 }}>Tree View</button>
-            <!-- TODO:  set download property dynamically -->
-            <a class=download part=download download="file.json" ${{
+                <!-- TODO:  set download property dynamically -->
+                <a class=download part=download download="file.json" ${{
     beObservant: {
         href: {
             observe: 'xtal-tree',
@@ -125,16 +126,16 @@ main{
         }
     }
 }}>
-                <svg viewBox="0 0 24 24" style="width:16.25px;height:16.25px">
-                    <g color="rgb(29, 155, 240)">
-                        <path stroke="currentcolor" d="M11.96 14.945c-.067 0-.136-.01-.203-.027-1.13-.318-2.097-.986-2.795-1.932-.832-1.125-1.176-2.508-.968-3.893s.942-2.605 2.068-3.438l3.53-2.608c2.322-1.716 5.61-1.224 7.33 1.1.83 1.127 1.175 2.51.967 3.895s-.943 2.605-2.07 3.438l-1.48 1.094c-.333.246-.804.175-1.05-.158-.246-.334-.176-.804.158-1.05l1.48-1.095c.803-.592 1.327-1.463 1.476-2.45.148-.988-.098-1.975-.69-2.778-1.225-1.656-3.572-2.01-5.23-.784l-3.53 2.608c-.802.593-1.326 1.464-1.475 2.45-.15.99.097 1.975.69 2.778.498.675 1.187 1.15 1.992 1.377.4.114.633.528.52.928-.092.33-.394.547-.722.547z"></path>
-                        <path stroke="currentcolor" d="M7.27 22.054c-1.61 0-3.197-.735-4.225-2.125-.832-1.127-1.176-2.51-.968-3.894s.943-2.605 2.07-3.438l1.478-1.094c.334-.245.805-.175 1.05.158s.177.804-.157 1.05l-1.48 1.095c-.803.593-1.326 1.464-1.475 2.45-.148.99.097 1.975.69 2.778 1.225 1.657 3.57 2.01 5.23.785l3.528-2.608c1.658-1.225 2.01-3.57.785-5.23-.498-.674-1.187-1.15-1.992-1.376-.4-.113-.633-.527-.52-.927.112-.4.528-.63.926-.522 1.13.318 2.096.986 2.794 1.932 1.717 2.324 1.224 5.612-1.1 7.33l-3.53 2.608c-.933.693-2.023 1.026-3.105 1.026z"></path>
-                    </g>
-                </svg>
-                Download
-            </a>
-            <button part=object-adder class="object adder" value=object
-            name="" ${{
+                    <svg viewBox="0 0 24 24" style="width:16.25px;height:16.25px">
+                        <g color="rgb(29, 155, 240)">
+                            <path stroke="currentcolor" d="M11.96 14.945c-.067 0-.136-.01-.203-.027-1.13-.318-2.097-.986-2.795-1.932-.832-1.125-1.176-2.508-.968-3.893s.942-2.605 2.068-3.438l3.53-2.608c2.322-1.716 5.61-1.224 7.33 1.1.83 1.127 1.175 2.51.967 3.895s-.943 2.605-2.07 3.438l-1.48 1.094c-.333.246-.804.175-1.05-.158-.246-.334-.176-.804.158-1.05l1.48-1.095c.803-.592 1.327-1.463 1.476-2.45.148-.988-.098-1.975-.69-2.778-1.225-1.656-3.572-2.01-5.23-.784l-3.53 2.608c-.802.593-1.326 1.464-1.475 2.45-.15.99.097 1.975.69 2.778.498.675 1.187 1.15 1.992 1.377.4.114.633.528.52.928-.092.33-.394.547-.722.547z"></path>
+                            <path stroke="currentcolor" d="M7.27 22.054c-1.61 0-3.197-.735-4.225-2.125-.832-1.127-1.176-2.51-.968-3.894s.943-2.605 2.07-3.438l1.478-1.094c.334-.245.805-.175 1.05.158s.177.804-.157 1.05l-1.48 1.095c-.803.593-1.326 1.464-1.475 2.45-.148.99.097 1.975.69 2.778 1.225 1.657 3.57 2.01 5.23.785l3.528-2.608c1.658-1.225 2.01-3.57.785-5.23-.498-.674-1.187-1.15-1.992-1.376-.4-.113-.633-.527-.52-.927.112-.4.528-.63.926-.522 1.13.318 2.096.986 2.794 1.932 1.717 2.324 1.224 5.612-1.1 7.33l-3.53 2.608c-.933.693-2.023 1.026-3.105 1.026z"></path>
+                        </g>
+                    </svg>
+                    Download
+                </a>
+                <button part=object-adder class="object adder" value=object
+                name="" ${{
     beNoticed: {
         click: {
             toNearestUpMatch: 'xtal-tree',
@@ -143,7 +144,7 @@ main{
         }
     }
 }}>+object</button>
-            <button part=string-adder class="string adder" value=string ${{
+                <button part=string-adder class="string adder" value=string ${{
     beNoticed: {
         click: {
             toNearestUpMatch: 'xtal-tree',
@@ -152,7 +153,7 @@ main{
         }
     }
 }}>+string</button>
-            <button part=bool-adder class="bool adder" value=bool ${{
+                <button part=bool-adder class="bool adder" value=bool ${{
     beNoticed: {
         click: {
             toNearestUpMatch: 'xtal-tree',
@@ -161,7 +162,7 @@ main{
         }
     }
 }}>+bool</button>
-            <button part=number-adder class="number adder" value=number ${{
+                <button part=number-adder class="number adder" value=number ${{
     beNoticed: {
         click: {
             toNearestUpMatch: 'xtal-tree',
@@ -170,7 +171,7 @@ main{
         }
     }
 }}>+number</button>
-            <button part=arr-adder class="arr adder" value=arr ${{
+                <button part=arr-adder class="arr adder" value=arr ${{
     beNoticed: {
         click: {
             toNearestUpMatch: 'xtal-tree',
@@ -179,10 +180,10 @@ main{
         }
     }
 }}>+array</button>
-        </menu>
-    </xtal-side-nav>
-    <h1 part=title >
-        <span ${{
+            </menu>
+        </xtal-side-nav>
+        <h1 part=title >
+            <span ${{
     beObservant: {
         textContent: {
             onSet: "readOnly",
@@ -192,11 +193,11 @@ main{
         }
     }
 }}>
-        </span>
-        
+            </span>
+            
 
-    </h1>
-    <label part=search-label class=search-label>Search:&nbsp;&nbsp;<input part=search-input type=search ${{
+        </h1>
+        <label part=search-label class=search-label>Search:&nbsp;&nbsp;<input part=search-input type=search ${{
     beNoticed: {
         input: {
             toNearestUpMatch: 'xtal-tree',
@@ -205,9 +206,9 @@ main{
         }
     }
 }}>
-    </label>
-    <div class=exp-coll>
-        <button part=expand-all class="action expand-all" aria-label="expand all" title="expand all" ${{
+        </label>
+        <div class=exp-coll>
+            <button part=expand-all class="action expand-all" aria-label="expand all" title="expand all" ${{
     beNoticed: {
         click: {
             toNearestUpMatch: 'xtal-tree',
@@ -216,7 +217,7 @@ main{
         }
     }
 }}>&nbsp;</button>
-        <button part=collapse-all class="action collapse-all" aria-label="collapse all" title="collapse all" ${{
+            <button part=collapse-all class="action collapse-all" aria-label="collapse all" title="collapse all" ${{
     beNoticed: {
         click: {
             toNearestUpMatch: 'xtal-tree',
@@ -225,7 +226,7 @@ main{
         }
     }
 }}>&nbsp;</button>
-        <button part=copy class="action copy" aria-label="copy" title="copy" name="" ${{
+            <button part=copy class="action copy" aria-label="copy" title="copy" name="" ${{
     beNoticed: {
         click: {
             toNearestUpMatch: 'xtal-tree',
@@ -234,23 +235,23 @@ main{
         }
     }
 }}>&nbsp;</button>
-    </div>
-</header>
-<!-- Tree View -->
-<template ${{
+        </div>
+    </header>
+    <!-- Tree View -->
+    <template ${{
     beSwitched: {
         if: '.treeView',
     }
 }}>
-    <xtal-vlist
-        part=xtal-vlist
-        class=animated 
-        style="width:100%;" 
-        timestamp-key="timestamp" 
-        id="vlist"
-        min-item-height=23
-        page-size=10
-        ${{
+        <xtal-vlist
+            part=xtal-vlist
+            class=animated 
+            style="width:100%;" 
+            timestamp-key="timestamp" 
+            id="vlist"
+            min-item-height=23
+            page-size=10
+            ${{
     beObservant: {
         list: { observe: "xtal-tree", vft: "viewableNodes" }
     },
@@ -294,7 +295,7 @@ main{
         }
     ]
 }}
-        ${{
+            ${{
     rowTransform: {
         divElements: [{}, {}, { "data-path": "path" }],
         fieldClasses: [{}, {}, { "style": "marginStyle" }],
@@ -319,10 +320,10 @@ main{
         }
     },
 }}
-    >
-       <style be-adopted="xtal-editor/list.css" slot=header></style>
-       <template slot=row>
-            <div class=field data-readonly debug part=field itemscope ${{
+        >
+        <style be-adopted="xtal-editor/list.css" slot=header></style>
+        <template slot=row>
+                <div class=field data-readonly debug part=field itemscope ${{
     beObservant: {
         'data-readonly': {
             observeHostProp: 'readOnly',
@@ -331,70 +332,70 @@ main{
         }
     }
 }}>
-                <div class=text-editing>
-                    <button class="expander" part=expander>.</button>
-                    <label part=key class=key></label>
-                    <input arial-label=value class=value part=value be-composed='{
-                        "dispatch":{
-                            "change": {
-                                "as": "c2b4531e-993d-4109-84fe-b9af0fb45927",
-                                "bubbles": true,
-                                "composed": true
-                            }
-                        }
-                    }'>
-                </div>
-                <div class=buttons>
-                    <section class=adder-buttons  part=adder-buttons >
-                        <template class=adder-template be-lazy='{
-                            "transform": {
-                                "button": [{"name": "path"}, {}, {}]
+                    <div class=text-editing>
+                        <button class="expander" part=expander>.</button>
+                        <label part=key class=key></label>
+                        <input arial-label=value class=value part=value be-composed='{
+                            "dispatch":{
+                                "change": {
+                                    "as": "c2b4531e-993d-4109-84fe-b9af0fb45927",
+                                    "bubbles": true,
+                                    "composed": true
+                                }
                             }
                         }'>
-                                <button part=object-adder class="object adder" value=object>+object</button>
-                                <button part=string-adder class="string adder" value=string>+string</button>
-                                <button part=bool-adder class="bool adder" value=bool>+bool</button>
-                                <button part=number-adder class="number adder" value=number>+number</button>
-                                <button part=arr-adder class="arr adder" value=arr>+array</button>
-                        </template>
-                    </section>
-                    <section class=exp-collapse-buttons>
-                        <template class=exp-coll-template be-lazy='{
+                    </div>
+                    <div class=buttons>
+                        <section class=adder-buttons  part=adder-buttons >
+                            <template class=adder-template be-lazy='{
                                 "transform": {
                                     "button": [{"name": "path"}, {}, {}]
                                 }
                             }'>
-                                <button class="action expand-all" aria-label="expand all" title="expand all">&nbsp;</button>
-                                <button class="action collapse-all" aria-label="collapse all" title="collapse all">&nbsp;</button>
-                        </template>
-                    </section>
-                    <section class=other-buttons>
-                        <button class="action copy" aria-label="copy" title="copy">&nbsp;</button>
-                    </section>
-                    <section class=delete-button>
-                        <button class="action delete" aria-label="delete" title="delete">&times;</button>
-                    </section>
+                                    <button part=object-adder class="object adder" value=object>+object</button>
+                                    <button part=string-adder class="string adder" value=string>+string</button>
+                                    <button part=bool-adder class="bool adder" value=bool>+bool</button>
+                                    <button part=number-adder class="number adder" value=number>+number</button>
+                                    <button part=arr-adder class="arr adder" value=arr>+array</button>
+                            </template>
+                        </section>
+                        <section class=exp-collapse-buttons>
+                            <template class=exp-coll-template be-lazy='{
+                                    "transform": {
+                                        "button": [{"name": "path"}, {}, {}]
+                                    }
+                                }'>
+                                    <button class="action expand-all" aria-label="expand all" title="expand all">&nbsp;</button>
+                                    <button class="action collapse-all" aria-label="collapse all" title="collapse all">&nbsp;</button>
+                            </template>
+                        </section>
+                        <section class=other-buttons>
+                            <button class="action copy" aria-label="copy" title="copy">&nbsp;</button>
+                        </section>
+                        <section class=delete-button>
+                            <button class="action delete" aria-label="delete" title="delete">&times;</button>
+                        </section>
+                    </div>
                 </div>
-            </div>
-        </template>
-    </xtal-vlist>
+            </template>
+        </xtal-vlist>
 
-</template>
-<!-- Text View -->
-<template ${{
+    </template>
+    <!-- Text View -->
+    <template ${{
     beSwitched: {
         if: { onSet: "textView", vft: "textView" }
     }
 }}>
-    <template be-active>
-        <script id=@power-elements/json-viewer/json-viewer.js></script>
-    </template>
-    <json-viewer class=animated ${{
+        <template be-active>
+            <script id=@power-elements/json-viewer/json-viewer.js></script>
+        </template>
+        <json-viewer class=animated ${{
     beObservant: {
         object: { vft: "value", parseValAs: "object" }
     }
 }}></json-viewer>
-</template>
+    </template>
 </main>
 <be-hive></be-hive>
 `;
